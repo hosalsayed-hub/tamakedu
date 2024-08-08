1b17b7c54d6774e6cf22b8283e4f9d88d876b888
hosalsayed-hub <128558834+hosalsayed-hub@users.noreply.github.com>
2024-08-08 22:54:25 +0000

    theard

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-threads threads_count, threads_count
-workers ENV.fetch("WEB_CONCURRENCY") {4}
-port        ENV.fetch("PORT") { 3000 }
-environment ENV.fetch("RAILS_ENV") { "development" }
-#plugin :tmp_restart
-get 'render/index'
-   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-   # Can be used by load balancers and uptime monitors to verify that the app is live.
-   get "up" => "rails/health#show", as: :rails_health_check
-
-   # Defines the root path route ("/")
-   root "render#index"
+RRails.application.routes.draw do
+    get 'render/index'
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+    # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+    # Can be used by load balancers and uptime monitors to verify that the app is live.
+    get "up" => "rails/health#show", as: :rails_health_check
+  
+    # Defines the root path route ("/")
+    root "render#index"
+  end
