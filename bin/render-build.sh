bcfd137ca32f894d5a868a79498c16d6d2d0a4a7
hosalsayed-hub <128558834+hosalsayed-hub@users.noreply.github.com>
2024-08-08 22:27:50 +0000

    cccc

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # exit on error
 bundle install
-bundle exec rails db:drop 
-bundle exec rails db:create
+bundle exec rails db:drop   --trace
+bundle exec rails db:create --trace
 bundle exec rails db:migrate --trace
-bundle exec rails db:seed 
+bundle exec rails db:seed  --trace
  
