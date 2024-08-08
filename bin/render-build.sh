060abd6f1a98ac56641cdec72c44f5b8bc178ef8
hosalsayed-hub <128558834+hosalsayed-hub@users.noreply.github.com>
2024-08-08 23:34:16 +0000

    magri

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 # exit on error
 bundle install
-bundle exec rails db:drop   --trace
-bundle exec rails db:create --trace
-bundle exec rails db:migrate --trace
-bundle exec rails db:seed  --trace
+bundle exec rails db:migrate 
  
